f1ca8a05d0a7d202fae976ca2e6f828a9701138b
BuildTools <unconfigured@null.spigotmc.org>
2019-03-16 10:00:51 -0400

    Finished up most of pomodoro clock (didn't include code because it was on codepen, oops). I did a code review for the Responsive Web Design projects to get them all up to date. Next are the Javascript algorithms and Data Structures.

--- 1. Responsive Web Design Projects/5. Build a Personal Portfolio Webpage/main.js
+++ 1. Responsive Web Design Projects/5. Build a Personal Portfolio Webpage/main.js
@@ -1,1 +1,91 @@
-console.log("Main.JS Linked and initialized.");
+$(document).ready(function () {
+
+    // Lightbox code.
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+    /* Click buttons for additional data links */
+    $("#btn_linkedin").click(function() {
+        window.open('https://www.linkedin.com/in/darsd/', '_blank');
+        return;
+    });
+
+    $("#btn_freeCodeCamp").click(function() {
+        window.open('https://www.freecodecamp.org/donald', '_blank');
+        return;
+    });
+
+    $("#btn_github").click(function() {
+        window.open('https://github.com/Destro168', '_blank');
+        return;
+    });
+
+    /*
+        Form related code.
+    */
+
+    // Get the form.
+    var form = $('#ajax-contact');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+    
+    function hideFormSubmissionText() {
+        $('#submit_error').hide(100);
+        $('#submit_success').hide(100);
+    }
+
+    hideFormSubmissionText();
+
+    // Set up an event listener for the contact form.
+    $(form).submit(function (event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // Always make sure form submission text is hidden.
+        hideFormSubmissionText();
+
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: formData
+            })
+            .done(function (response) {
+                // Make sure that the formMessages div has the 'success' class.
+                $(formMessages).removeClass('error');
+                $(formMessages).addClass('success');
+
+                // Set the message text.
+                $(formMessages).text(response);
+
+                // Clear the form.
+                $('#name').val('');
+                $('#email').val('');
+                $('#message').val('');
+
+                // Display toastr success message to user.
+                toastr.info('Email Sent - Thank you for contacting me, I will respond within 24 hours.');
+                $('#exampleModal').modal('hide');
+            })
+            .fail(function (data) {
+                // Make sure that the formMessages div has the 'error' class.
+                $('#submit_error').show(100);
+                setTimeout(hideFormSubmissionText, 10000);
+
+                // Set the message text.
+                if (data.responseText !== '') {
+                    $(formMessages).text(data.responseText);
+                } else {
+                    $(formMessages).text(
+                        'Oops! An error occured and your message could not be sent.');
+                }
+            });
+
+        return false;
+    });
+});
--- 3. Front End Libraries Projects/Pomodoro Clock/pomodoro_container.js
+++ 3. Front End Libraries Projects/Pomodoro Clock/pomodoro_container.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const e = React.createElement;
-
 /**
  * Variables needed:
  * 
@@ -13,9 +11,12 @@ const e = React.createElement;
  * remainingMins = 25
  * remainingSeconds = 5
  * 
- * Whether or not the timer is active.
- * timerIsActive = false
+ * The state of the timer.
+ * timerState = 'off', 'on', 'pause'
+ * timerType: 'Session', 'Break'
  */
+
+let state = {};
 
 // Main React Class
 class Pomodoro extends React.Component {
@@ -24,8 +25,17 @@ class Pomodoro extends React.Component {
     super(props);
 
     this.state = {
-      timeMinutes: 0,
-      timeSeconds: 0
+      timerState: 'off',
+      timerType: 'Session',
+      remainingBreaks: 3,
+      userInput: {
+        sessionLength: 1,
+        breakLength: 1
+      },
+      remainingTime: {
+        mins: 25,
+        secs: 0
+      }
     };
 
     this.clickStartStop = this.clickStartStop.bind(this);
@@ -38,6 +48,22 @@ class Pomodoro extends React.Component {
     this.sessionIncrement = this.sessionIncrement.bind(this);
   }
   
+  // Helper functions to change timer state.
+  setTimerStateOff() {
+    state["timerState"] = 'off';
+    this.setState(state);
+  }
+
+  setTimerStateOn() {
+    state["timerState"] = 'on';
+    this.setState(state);
+  }
+
+  setTimerStatePause() {
+    state["timerState"] = 'pause';
+    this.setState(state);
+  }
+
   /**
    * User Story #18: When I first click the element with id="start_stop", the timer should begin running from the value currently displayed in
    * id="session-length", even if the value has been incremented or decremented from the original value of 25.
@@ -55,7 +81,86 @@ class Pomodoro extends React.Component {
    *  an HTML5 audio tag and have a corresponding id="beep".
    */
   clickStartStop() {
+    state = Object.assign({}, this.state);
     
+    // #20-26
+    if (this.state["timerState"] == 'off' || this.state["timerState"] == 'pause') {
+      // #18 - If timer is being turned on first time, then set remaining time to sessionLength.
+      if (this.state["timerState"] == 'off') {
+        state["remainingTime"]["mins"] = state["userInput"]["sessionLength"];
+        state["remainingTime"]["secs"] = 0;
+      }
+
+      // Set time state to on.
+      state["timerState"] = 'on';
+
+      this.setTimerStateOn();
+
+      // We use a series of functions here to organize recursive code.
+      // doTimeDecrease: Actually performs operations to chnage time variables to decrease time by 1.
+      // doClockTick: Function that calls itself every second until timer is off.
+      function doTimeDecrease(boundFunction) {
+        if (state["remainingTime"]["secs"] > 0) {
+          state["remainingTime"]["secs"] = state["remainingTime"]["secs"] - 1;
+
+          if (state["remainingTime"]["secs"] == 0) {
+            (state["timerType"] == 'Session') ? state["timerType"] = 'Break' : state["timerType"] = 'Session';
+          }
+        }
+        else if (state["remainingTime"]["mins"] > 0) {
+          state["remainingTime"]["mins"] = state["remainingTime"]["mins"] - 1;
+          state["remainingTime"]["secs"] = 59;
+        }
+        // Otherwise, handle processing for switching from session to break, or break to off.
+        else {
+          // If there are breaks remaining, keep alternating sessions and break;
+          if (state["remainingBreaks"] > 0) {
+            
+            // If a session just ended, do a break.
+            if (state["timerType"] == 'Session') {
+              state["remainingTime"]["mins"] = state["userInput"]["sessionLength"];
+              state["remainingTime"]["secs"] = 0;
+              state["remainingBreaks"] -= 1;
+
+              console.log("Track Session: ", state["timerType"], state["userInput"]["sessionLength"], state["remainingTime"]["mins"], state["remainingTime"]["secs"]);
+            }
+
+            // If a break just ended, do a session.
+            else if (state["timerType"] == 'Break') {
+              state["remainingTime"]["mins"] = state["userInput"]["breakLength"];
+              state["remainingTime"]["secs"] = 0;
+              
+              console.log("Track Break: ", state["timerType"], state["userInput"]["breakLength"], state["remainingTime"]["mins"], state["remainingTime"]["secs"]);
+            }
+          }
+          // Otherwise, end.
+          else {
+            state["timerType"] = 'Session';
+            state["timerState"] = 'off';
+
+            // Play the alarm.
+          }
+        }
+        
+        boundFunction.setState(state);
+      }
+      
+      function doClockTick(boundFunction) {
+        setTimeout(function() {
+          if (boundFunction.state["timerState"] == 'on') {
+            doTimeDecrease(boundFunction);
+            doClockTick(boundFunction);
+          }
+        }, 1000);
+      }
+      
+      doClockTick(this);
+    }
+
+    // 20.
+    else if (this.state["timerState"] == 'on') {
+      this.setTimerStatePause();
+    }
   }
 
   /**
@@ -64,15 +169,25 @@ class Pomodoro extends React.Component {
    * User Story #28: The audio element with id of beep must stop playing and be rewound to the beginning when the element with the id of reset is clicked.
    */
   clickReset() {
-    // Stop any running timers.
+    // Reset all variables. (Pure copy of constructor variables).
+    let defaultState = {
+      timerState: 'off',
+      timerType: 'Session',
+      remainingBreaks: 3,
+      userInput: {
+        sessionLength: 25,
+        breakLength: 5
+      },
+      remainingTime: {
+        mins: 25,
+        secs: 0
+      }
+    }
 
-    // #break-length gets value reset to 5, the default.
+    // Update state.
+    this.setState(defaultState);
 
-    // #session-length gets value reset to 25, the default.
-
-    // #time-left returns to default state.
-
-    // Audio stops playing and is rewound to the begining.
+    // Audio stops playing and is rewound to the begining. (TODO)
   }
 
   /**
@@ -82,7 +197,15 @@ class Pomodoro extends React.Component {
    */
   breakDecrement() {
     // Decrements value of #break-decrement by 1.
+    let state = this.state;
+    
+    // #16.
+    if (state["userInput"]["breakLength"] <= 1) {
+      return;
+    }
 
+    state["userInput"]["breakLength"] -= 1;
+    this.setState(state);
   }
 
   /**
@@ -92,7 +215,15 @@ class Pomodoro extends React.Component {
    */
   breakIncrement() {
     // Increases value of #break-increment by 1.
+    let state = this.state;
 
+     // #17.
+     if (state["userInput"]["breakLength"] >= 60) {
+      return;
+    }
+
+    state["userInput"]["breakLength"] += 1;
+    this.setState(state);
   }
 
   /**
@@ -102,7 +233,15 @@ class Pomodoro extends React.Component {
    */
   sessionDecrement() {
     // Decrements value of #session-decrement by 1.
+    let state = this.state;
 
+    // #16.
+    if (state["userInput"]["sessionLength"] <= 1) {
+      return;
+    }
+
+    state["userInput"]["sessionLength"] -= 1;
+    this.setState(state);
   }
 
   /**
@@ -112,9 +251,22 @@ class Pomodoro extends React.Component {
    */
   sessionIncrement() {
     // Increments value of #session-increment by 1.
+    let state = this.state;
+    
+     // #17.
+     if (state["userInput"]["sessionLength"] >= 60) {
+      return;
+    }
 
+    state["userInput"]["sessionLength"] += 1;
+    this.setState(state);
   }
 
+  /** Private helper function to convert an integer time to a string with padding if less than 10. */
+  getFormattedTime(timeInt) {
+    return (timeInt < 10) ? ('0' + timeInt) : timeInt;
+  }
+  
   // User Story #19: If the timer is running, the element with the id of time-left should display the remaining time in mm:ss format
   // (decrementing by a value of 1 and updating the display every 1000ms).
   //  Solution: Add a variable to p#time-left that is updated during the #start_stop onclick function (toggleActive). 
@@ -123,34 +275,62 @@ class Pomodoro extends React.Component {
   render() {
     return (
       <div id="main">
-        <h1 id="title">Pomodor Clock</h1>
+        <h1 id="title">Pomodoro Clock</h1>
         <h3>
-          Author's Note: TBC
+          Author's Note: The highlight of this project was really the count-down function for time.
+
+          It required me to demonstrate mastery of recusion and state scope to implement a proper solution.
+
+          The solution that I created recursively calls a count-down function while remaining state aware due to
+          the parent function being passed as the argument 'this'. This lets the count-down function exit properly,
+          rather than keep calling itself, once the timer's state changes to anything other than 'on'.
+
+          A few traps that I fell into while coding this were to use a while loop to attempt to handle the count-down. Doing this
+          even with a setTimeout() function fails because you will constantly call the setTimeout function even before the first one
+          runs.
+
+          And, the second trap that I fell into was using a local state variable, rather than 'this' to pass state information
+          to the count-down function.
+
+          Thankfully, neither trap required much time or effort to resolve. This was a very fun challenge overall!
         </h3>
         <div id="clock">
+          <p id="session-label">Session Length</p>
+          <button id="session-increment" onClick={this.sessionIncrement}>Session +</button>
+          <button id="session-decrement" onClick={this.sessionDecrement}>Session -</button>
 
-          <p id="break-label"></p>
-          <p id="session-label"></p>
+          <p id="break-label">Break Length</p>
+          <button id="break-increment" onClick={this.breakIncrement}>Break +</button>
+          <button id="break-decrement" onClick={this.breakDecrement}>Break -</button><br /><br />
+          
+          <p id="session-length" defaultValue="25">{this.state.userInput.sessionLength}</p>
+          <p id="break-length" defaultValue="5">{this.state.userInput.breakLength}</p>
 
-          <button id="break-decrement"></button>
-          <button id="session-decrement"></button>
+          <p id="timer-label" defaultValue="Session">{this.state.timerType}</p>
           
-          <button id="break-increment"></button>
-          <button id="session-increment"></button>
+          <p id="time-left">{this.getFormattedTime(this.state.remainingTime.mins)}:{this.getFormattedTime(this.state.remainingTime.secs)}</p>
+          <p id="timer-state">Timer State: {this.state.timerState} / Type: {this.state.timerType} / RemainingBreaks: {this.state.remainingBreaks}</p>
 
-          <p id="break-length" defaultValue="5"></p>
-          <p id="session-length" defaultValue="25"></p>
-          <p id="timer-label"></p>
-          
-          <p id="time-left"></p>
-          
-          <button id="start_stop" onclick={this.clickStartStop}></button>
-          <button id="reset" onClick={this.clickReset}></button>
+          <button id="start_stop" onClick={this.clickStartStop}>Start/Stop</button>
+          <button id="reset" onClick={this.clickReset}>Reset</button>
         </div>
       </div>
     );
   }
 }
 
+/*
+this.state = {
+  timerState: 'off',
+  userInput: {
+    sessionLength: 25,
+    breakLength: 5
+  },
+  remainingTime: {
+    mins: 25,
+    secs: 5
+  }
+*/
+
 const domContainer = document.querySelector('#pomodoro_container');
-ReactDOM.render(e(Pomodoro), domContainer);
+ReactDOM.render(<Pomodoro />, domContainer);
